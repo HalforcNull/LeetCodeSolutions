7c416e2298922cede4472af452ffe6849d1e9200
Halforc Null <yaorunan@gmail.com>
2017-01-26 14:39:25 -0600

    Add 125 198

---
+++ LeetCodeSolution/LeetCodeSolution/Solution125.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution125
+    {
+        public bool IsPalindrome(string s)
+        {
+            List<char> cList = s.ToLower().ToList().FindAll(c => Char.IsLetter(c)||Char.IsDigit(c));
+            if(cList.Count < 2)
+            {
+                return true;
+            }
+
+            int up = cList.Count - 1;
+
+            for (int i = 0; i < cList.Count / 2; i++)
+            {
+                if(cList[i] != cList[up-i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCodeSolution/Solution198.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution198
+    {
+        public int Rob(int[] nums)
+        {
+            int chooseMax = 0;
+            int notChooseMax = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int chooseThenNot = chooseMax;
+                int notchooseThenNot = notChooseMax;
+                int notchooseThenChoose = notChooseMax + nums[i];
+                if(chooseThenNot > notchooseThenNot)
+                {
+                    notChooseMax = chooseThenNot;
+                }
+                chooseMax = notchooseThenChoose;
+            }
+
+            if(chooseMax > notChooseMax)
+            {
+                return chooseMax;
+            }
+            else
+            {
+                return notChooseMax;
+            }
+        }
+    }
+}
