4889ef1a10f5643efa2c91a77d81e9c3dc95737c
Halforc Null <yaorunan@gmail.com>
2017-02-01 11:58:06 -0600

    Solution20

---
+++ LeetCodeSolution/LeetCodeSolution/Solution20.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution20
+    {
+        /// <summary>
+        /// classical question for stack
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsValid(string s)
+        {
+            Stack<char> st = new Stack<char>();
+            for(int i = 0; i < s.Length; i++)
+            {
+                switch(s[i])
+                {
+                    case '(':
+                    case '[':
+                    case '{':
+                        st.Push(s[i]);
+                        continue;
+                    case ')':
+                        if(st.Count == 0 || st.Pop() != '(')
+                        { return false; }
+                        continue;
+                    case ']':
+                        if (st.Count == 0 || st.Pop() != '[')
+                        { return false; }
+                        continue;
+                    case '}':
+                        if (st.Count == 0 || st.Pop() != '{')
+                        { return false; }
+                        continue;
+                    default:
+                        return false;
+                }
+            }
+
+            if(st.Count != 0)
+            { return false; }
+            else
+            { return true; }
+        }
+        
+    }
+}
