af4f4edf894359d673cdc698d8523bc743f9b73c
Halforc Null <yaorunan@gmail.com>
2017-02-01 14:40:55 -0600

    Solution 387

---
+++ LeetCodeSolution/LeetCodeSolution/Solution387.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{   
+    public class Solution387
+    {
+        public int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> chCount = new Dictionary<char, int>();
+            Dictionary<char, int> chIndex = new Dictionary<char, int>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if(!chCount.ContainsKey(c) )
+                {
+                    chCount.Add(c, 1);
+                    chIndex.Add(c, i);
+                }
+                else
+                {
+                    chCount[c]++;
+                }
+            }
+
+            if (chCount.Count == 0)
+            { return -1; }
+
+            int index = -1;
+            foreach(KeyValuePair<char,int> c in chCount)
+            {
+                if(c.Value != 1)
+                { continue; }
+
+                if(index < 0 || chIndex[c.Key] < index)
+                { index = chIndex[c.Key]; }
+            }
+
+            return index;
+        }
+    }
+}
