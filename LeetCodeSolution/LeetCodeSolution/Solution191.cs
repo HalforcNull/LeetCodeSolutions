7b625a00877549cd2287dac2b9a0153d3a492191
Halforc Null <yaorunan@gmail.com>
2017-02-01 12:34:24 -0600

    Solution 191

---
+++ LeetCodeSolution/LeetCodeSolution/Solution191.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution191
+    {
+        public int HammingWeight(uint n)
+        {
+            uint remain = n;
+            int result = 0;
+            while(remain != 0)
+            {
+                if(remain % 2 == 1)
+                { result++; }
+                remain = remain / 2;
+            }
+
+            return result;
+        }
+    }
+}
