67b599503223dd3e180cc1bfec4c2c2947f63b80
Halforc Null <yaorunan@gmail.com>
2017-02-01 14:04:52 -0600

    Solution189

---
+++ LeetCodeSolution/LeetCodeSolution/Solution189.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution189
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            int length = nums.Length;
+            if (length == 0 || k % length == 0)
+            {
+                return;
+            }
+
+            int startI = 0;
+            int currentI = 0;
+            int currentV = nums[0];
+
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int nextI = (currentI + k) % length;
+                int nextV = nums[nextI];
+                nums[nextI] = currentV;
+
+
+                currentI = nextI;
+                currentV = nextV;
+                if (currentI == startI)
+                {
+                    startI++;
+                    currentI = startI % length;
+                    currentV = nums[currentI];
+                }
+            }
+
+
+            return;
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCodeSolutionTests/Solution189Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution.Tests
+{
+    [TestClass()]
+    public class Solution189Tests
+    {
+        [TestMethod()]
+        public void RotateTest()
+        {
+            Solution189 s = new Solution189();
+            int[] input = { 1, 2 };//, 3, 4, 5, 6 };
+            s.Rotate(input, 1);
+        }
+    }
+}
