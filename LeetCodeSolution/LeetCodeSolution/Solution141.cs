af7d88f80fccffd3adcb1eab6eff209fa7412279
Halforc Null <yaorunan@gmail.com>
2017-02-01 00:27:41 -0600

    solution141

---
+++ LeetCodeSolution/LeetCodeSolution/Solution141.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    class Solution141
+    {
+        public class Solution
+        {
+            public bool HasCycle(ListNode head)
+            {
+                if(head == null)
+                {
+                    return false;
+                }
+
+                ListNode current = head.next;
+                while(current!=null)
+                {
+                    if(current == head)
+                    {
+                        return true;
+                    }
+
+                    // if not using extra space, we need add some 'mark' on original node. 
+                    // in my case, i simple point the next node of any tested node to head.
+                    ListNode temp = current.next;
+                    current.next = head;
+                    current = temp;
+                }
+
+                return false;
+            }
+        }
+    }
+}
