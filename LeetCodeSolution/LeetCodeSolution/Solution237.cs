e4a97f0b01b9e2a6b48eb491ddca7ac9f319a798
Halforc Null <yaorunan@gmail.com>
2017-02-01 00:45:12 -0600

    solution237

---
+++ LeetCodeSolution/LeetCodeSolution/Solution237.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution237
+    {
+        public void DeleteNode(ListNode node)
+        {
+            ListNode t = node.next;
+
+            if (t == null)
+            { return; }
+
+            node.val = t.val;
+            node.next = t.next;
+
+            return;
+        }
+    }
+}
