647f37fa066e4d05cb91884507899a2fac67a92f
Halforc Null <yaorunan@gmail.com>
2017-02-01 12:54:41 -0600

    Solution 26

---
+++ LeetCodeSolution/LeetCodeSolution/Solution26.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution26
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if(nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int InsertIndex = 1;
+            int current = nums[0];
+
+            for(int i = 1; i < nums.Length;i++)
+            {
+                if(current != nums[i])
+                {
+                    nums[InsertIndex] = nums[i];
+                    current = nums[i];
+                    InsertIndex++;
+                    continue;
+                }
+            }
+
+            return InsertIndex;
+        }
+    }
+}
