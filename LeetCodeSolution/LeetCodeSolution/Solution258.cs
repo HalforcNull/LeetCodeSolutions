1f4cb5335d8e5018db0d540eb89a8308bf8dd6d0
Halforc Null <yaorunan@gmail.com>
2017-02-01 15:47:03 -0600

    Solution 258

---
+++ LeetCodeSolution/LeetCodeSolution/Solution258.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution258
+    {
+        public int AddDigits(int num)
+        {
+            int current = num;
+            while(current > 9)
+            {
+                int temp = current;
+                int result = 0;
+                while(temp > 0)
+                {
+                    result += temp % 10;
+                    temp = temp / 10;
+                }
+                current = result;
+            }
+            return current;
+        }
+    }
+}
