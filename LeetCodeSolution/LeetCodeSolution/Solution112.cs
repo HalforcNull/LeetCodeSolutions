61d44bb3afd1e90f6bab6bad875266d51e90b80c
Halforc Null <yaorunan@gmail.com>
2017-02-01 21:02:39 -0600

    Solution112

---
+++ LeetCodeSolution/LeetCodeSolution/Solution112.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution112
+    {
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return sum == root.val;
+            }
+
+            return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+        }
+    }
+}
