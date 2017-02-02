938c63175ce51187356b872ec0589268a39cc7fc
Halforc Null <yaorunan@gmail.com>
2017-02-01 20:58:13 -0600

    solution101

---
+++ LeetCodeSolution/LeetCodeSolution/Solution101.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution101
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            List<TreeNode> TreeNodes = new List<TreeNode>();
+            if (root == null)
+            {
+                return true;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return true;
+            }
+
+            return checkNodes(root.left, root.right);
+        }
+
+        public bool checkNodes(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.val == right.val && checkNodes(left.left, right.right) && checkNodes(left.right, right.left);
+        }
+    }
+}
