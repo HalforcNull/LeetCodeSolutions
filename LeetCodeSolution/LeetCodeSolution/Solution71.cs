967185633be390e824de5362e501daaeb811b000
Halforc Null <yaorunan@gmail.com>
2017-02-02 16:33:07 -0600

    Solution 71

---
+++ LeetCodeSolution/LeetCodeSolution/Solution71.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution71
+    {
+        /*
+         / -> root
+        /a -> in (a)
+        . -> THIS dir path 
+        /a/./ -> still in /a
+        /a/./b -> in /a/b
+        .. -> go "up" one level
+        */
+
+
+        public string SimplifyPath(string path)
+        {
+            string result = "";
+            Stack<string> pathes = new Stack<string>();
+            string[] commands = path.Split("/".ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach(string s in commands)
+            {
+                if(s.Length == 1 && s[0] == '.')
+                {
+                    continue;
+                }
+
+                if(s.Length == 2&& s[0] == '.' && s[1] == '.' )
+                {
+                    if (pathes.Count > 0)
+                    { pathes.Pop(); }
+                    continue;
+                }
+
+                pathes.Push(s);
+            }
+
+            while(pathes.Count > 0)
+            {
+                result = "/" + pathes.Pop() +  result;
+            }
+
+            if(result.Length==0)
+            {
+                result = "/";
+            }
+
+            return result;
+        }
+    }
+}
