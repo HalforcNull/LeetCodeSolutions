907d7cf8c509f8ed940c17e971072933a5223959
Halforc Null <yaorunan@gmail.com>
2017-02-02 00:08:30 -0600

    Solution 75 LeetCode eror

---
+++ LeetCodeSolution/LeetCodeSolution/Solution75.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution75
+    {
+        public void SortColors(int[] nums)
+        {
+            int countOfRed = 0;
+            int countOfWhite = 0;
+            int countOfBlue = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == 0)
+                { countOfRed++; continue; }
+
+                if(nums[i] == 1)
+                { countOfWhite++; continue; }
+
+                countOfBlue++;
+            }
+
+
+            for(int i = 0; i < nums.Length;i++)
+            {
+                if(i<countOfRed)
+                {
+                    nums[i] = 0;
+                    continue;
+                }
+
+                if (i < countOfRed+ countOfWhite)
+                {
+                    nums[i] = 1;
+                    continue;
+                }
+
+                nums[i] = 2;
+            }
+
+            /* works in Visual Studio, error in Leetcode
+            nums = Enumerable.Repeat(0, countOfRed).Concat(
+                        Enumerable.Repeat(1, countOfWhite).Concat(
+                            Enumerable.Repeat(2, countOfBlue)
+                            )
+                        ).ToArray();
+            */
+            
+            return;
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCodeSolutionTests/Solution75Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution.Tests
+{
+    [TestClass()]
+    public class Solution75Tests
+    {
+        [TestMethod()]
+        public void SortColorsTest()
+        {
+            Solution75 s = new Solution75();
+            int[] nums = { 1, 0 };
+            s.SortColors(nums);
+        }
+    }
+}
