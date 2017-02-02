59c0735945e7bba9bac8e94fe6bb0b961180e16f
Halforc Null <yaorunan@gmail.com>
2017-02-02 14:54:30 -0600

    Add Quick Select Solution for 215

--- LeetCodeSolution/LeetCodeSolution/Solution215.cs
+++ LeetCodeSolution/LeetCodeSolution/Solution215.cs
@@ -7,11 +7,56 @@ namespace LeetCodeSolution
 {
     public class Solution215
     {
-        public int FindKthLargest(int[] nums, int k)
+        public int CSharpLazySolution(int[] nums, int k)
         {
             List<int> t = nums.ToList();
             t.Sort();
-            return t[t.Count-k];
+            return t[t.Count - k];
         }
+
+        /// <summary>
+        /// Quick select option
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int FindKthLargest(int[] nums, int k)
+        {
+            List<int> LowerRow = new List<int>();
+            List<int> HigerRow = new List<int>();
+
+            Random r = new Random();
+            int pointer = r.Next(nums.Length);
+            int pivot = nums[pointer];
+            
+            foreach(int num in nums)
+            {
+                if(num<pivot)
+                {
+                    LowerRow.Add(num);
+                    continue;
+                }
+                 
+                if(num>pivot)
+                {
+                    HigerRow.Add(num);
+                    continue;
+                }
+            }
+
+            if(k<= HigerRow.Count)
+            {
+                return FindKthLargest(HigerRow.ToArray(), k);
+            }
+
+            if(k>nums.Length - LowerRow.Count)
+            {
+                return FindKthLargest(LowerRow.ToArray(), k - nums.Length + LowerRow.Count);
+            }
+
+            return pivot;
+        }
+
+
     }
 }
---
+++ LeetCodeSolution/LeetCodeSolutionTests/Solution215Tests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution.Tests
+{
+    [TestClass()]
+    public class Solution215Tests
+    {
+        [TestMethod()]
+        public void FindKthLargestTest()
+        {
+            Solution215 s = new Solution215();
+            int[] nums = { 2,1 };
+            s.FindKthLargest(nums, 2);
+        }
+    }
+}
