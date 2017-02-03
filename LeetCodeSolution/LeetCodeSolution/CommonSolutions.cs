3c85b501ac3d2ae3e73a2be196c6954332bf6a2c
Halforc Null <yaorunan@gmail.com>
2017-02-03 00:47:04 -0600

    Solution 15 and K sum -- still have bug

---
+++ LeetCodeSolution/LeetCodeSolution/CommonSolutions.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class CommonSolutions
+    {
+        #region K Sum
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sums"></param>
+        /// <param name="total"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public IList<IList<int>> KSum(int[] nums, int total, int k)
+        {
+            QuickSortWithRandomPivot(ref nums, 0, nums.Length -1 );
+
+            return KSumSub(ref nums, 0, total, k);
+        }
+
+        private IList<IList<int>> KSumSub(ref int[] nums, int start, int total, int k)
+        {
+            
+            if (k == 2)
+            {
+                return TwoSumSub(ref nums, start, total);
+            }
+
+            List<IList<int>> result = new List<IList<int>>();
+
+            int temp = nums[start];
+            IList<IList<int>> subresult = KSumSub(ref nums, start + 1, total - temp, k - 1);
+            subresult.ToList().ForEach(t => { t.Add(temp); result.Add(t); } );
+            
+            for(int i = start+1; i < nums.Length; i++)
+            {
+                if(nums[i] == temp)
+                { continue; }
+
+                temp = nums[i];
+                subresult = KSumSub(ref nums, i + 1, total - temp, k - 1);
+                subresult.ToList().ForEach(t => { t.Add(temp); result.Add(t); });
+            }
+
+            return result;
+            
+        }
+
+        private IList<IList<int>> TwoSumSub(ref int[] nums, int start, int total)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            Dictionary<int, int> resultHash = new Dictionary<int, int>();
+            int i = start;
+            int j = nums.Length - 1;
+            while(i < j)
+            {
+                int sum = nums[i] + nums[j];
+                if(sum == total)
+                {
+                    if(!resultHash.ContainsKey(nums[i]) )
+                    { resultHash.Add(nums[i], nums[j]); }
+                    i++;
+                    j--;
+                }
+                else if( sum< total)
+                { i++; }
+                else
+                { j--; }
+            }
+
+            foreach(KeyValuePair<int,int> r in resultHash)
+            {
+                List<int> t = new List<int>();
+                t.Add(r.Key);
+                t.Add(r.Value);
+
+                result.Add(t);
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region Quick Sort
+        public void QuickSortWithRandomPivot(ref int[] nums, int start, int end)
+        {
+
+            if(start >= end)
+            { return; }
+            int length = end - start + 1;
+
+            Random r = new Random();
+            int pIndex = r.Next(length);
+
+            int NewPIndex = Partition(ref nums, start, end, pIndex);
+            QuickSortWithRandomPivot(ref nums, start, NewPIndex - 1);
+            QuickSortWithRandomPivot(ref nums, NewPIndex + 1, end);
+
+        }
+
+        private int Partition(ref int[] nums, int start, int end, int pivotIndex)
+        {
+            int pValue = nums[pivotIndex];
+
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+
+            int storeIndex = start;
+            for(int i = start; i < end; i++)
+            {
+                if(nums[i] <= pValue)
+                {
+                    temp = nums[i];
+                    nums[i] = nums[storeIndex];
+                    nums[storeIndex] = temp;
+
+                    storeIndex++;
+                }
+            }
+
+            temp = nums[end];
+            nums[end] = nums[storeIndex];
+            nums[storeIndex] = temp;
+
+            return storeIndex;
+        }
+        #endregion
+    }
+}
---
+++ LeetCodeSolution/LeetCodeSolution/Solution15.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution15
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            CommonSolutions s = new CommonSolutions();
+            return s.KSum(nums, 0, 3);
+        }
+
+    }
+}
