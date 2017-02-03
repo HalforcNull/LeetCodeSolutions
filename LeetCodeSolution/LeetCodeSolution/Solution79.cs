4c62e6980c8144acaf3f185e799df047f99cb519
Halforc Null <yaorunan@gmail.com>
2017-02-03 02:53:16 -0600

    Solution 79

---
+++ LeetCodeSolution/LeetCodeSolution/Solution79.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution79
+    {
+        public bool Exist(char[,] board, string word)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    bool[][] path = new bool[board.GetLength(0)][];
+                    for(int k = 0; k < board.GetLength(0); k++)
+                    {
+                        path[k] = new bool[board.GetLength(1)];
+                    }
+
+                    if (IsExistStartWith(board, path, i, j, word))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool IsExistStartWith(char[,] board, bool[][] path, int i, int j, string word)
+        {
+            if(word.Length == 0)
+            {
+                return true;
+            }
+
+            if ( i < 0 || j < 0
+                || board.GetLength(0) <= i || board.GetLength(1) <= j
+                || path[i][j]
+                || word[0] != board[i, j])
+            {
+                return false;
+            }
+            path[i][j] = true;
+
+            return IsExistStartWith(board, path.Select(t => t.ToArray()).ToArray(), i + 1, j, word.Substring(1))
+                || IsExistStartWith(board, path.Select(t => t.ToArray()).ToArray(), i - 1, j, word.Substring(1))
+                || IsExistStartWith(board, path.Select(t => t.ToArray()).ToArray(), i, j + 1, word.Substring(1))
+                || IsExistStartWith(board, path.Select(t => t.ToArray()).ToArray(), i, j - 1, word.Substring(1));
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCodeSolutionTests/Solution79Tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution.Tests
+{
+    [TestClass()]
+    public class Solution79Tests
+    {
+        [TestMethod()]
+        public void ExistTest()
+        {
+            Solution79 s = new Solution79();
+            char[,] board =
+            {
+                {'A','B','C','E' },
+                {'S','F','C','S' },
+                {'A', 'D','E','E' }
+            };
+
+            s.Exist(board, "ABCCED");
+        }
+    }
+}
