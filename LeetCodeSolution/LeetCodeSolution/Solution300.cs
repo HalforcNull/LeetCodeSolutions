abc54d828e9a86ebc76ccf169b2a592a92e55087
Halforc Null <yaorunan@gmail.com>
2017-02-03 08:36:41 -0600

    Solution 300.cs

---
+++ LeetCodeSolution/LeetCodeSolution/Solution300.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution300
+    {
+        //https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+        public int LengthOfLIS(int[] nums)
+        {
+            if(nums.Length == 0)
+            { return 0; }
+            List<int> selection = new List<int>();
+            selection.Add(nums[0]);
+            for(int i = 1; i < nums.Length; i++)
+            {
+                int v = nums[i];
+
+                if(selection.Last() < v)
+                {
+                    selection.Add(v);
+                    continue;
+                }
+
+                for(int j = 0; j < selection.Count; j++)
+                {
+                    if(selection[j] >= v)
+                    {
+                        selection[j] = v;
+                        break;
+                    }
+                }
+            }
+
+            return selection.Count();
+        }
+    }
+}
