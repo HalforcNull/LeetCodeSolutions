6f8ce51252150450868db6c1c5baae8e1fde197f
Halforc Null <yaorunan@gmail.com>
2017-02-03 08:49:20 -0600

    Solution114

---
+++ LeetCodeSolution/LeetCodeSolution/Solution114.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution114
+    {
+        public void Flatten(TreeNode root)
+        {
+            if(root ==null)
+            { return ; }
+            FlattenSub(root);
+        }
+
+        public TreeNode FlattenSub(TreeNode root)
+        {
+            if(root.left == null && root.right ==null)
+            {
+                return root;
+            }
+
+            TreeNode oLeft = root.left;
+            TreeNode oRight = root.right;
+
+            root.left = null;
+            if(oLeft == null)
+            {
+                return FlattenSub(oRight);
+            }
+
+            if(oRight == null)
+            {
+                root.right = oLeft;
+                return FlattenSub(oLeft);
+            }
+
+            root.right = oLeft;
+            FlattenSub(oLeft).right = oRight;
+            return FlattenSub(oRight);
+        }
+    }
+}
