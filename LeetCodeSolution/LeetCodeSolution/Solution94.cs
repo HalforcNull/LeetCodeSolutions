4af0f4def009b58dd713a91e4f8123423e99f39f
Halforc Null <yaorunan@gmail.com>
2017-02-03 05:53:30 -0600

    Solution94

---
+++ LeetCodeSolution/LeetCodeSolution/Solution94.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeSolution
+{
+    public class Solution94
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if(root == null)
+            {
+                return new List<int>();
+            }
+
+            IList<int> leftResult = InorderTraversal(root.left);
+            IList<int> rightResult = InorderTraversal(root.right);
+
+            leftResult.Add(root.val);
+            foreach (int i in rightResult)
+            { leftResult.Add(i); }
+
+            return leftResult;
+        }
+
+    }
+}
